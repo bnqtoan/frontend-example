2cbd63a97f716cf59e02f3cf423ea7ac42186bec
Toàn <bnqtoan@gmail.com>
2022-01-14 17:15:17 +0700

    add tailwind-wow

---
+++ tailwind-wow/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "**/*.html"
+  ],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    
+  ],
+}
